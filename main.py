d2911b595c97bc02d08005b7290da4f9f46e6127
Gabriel Barbosa <gabriel.vitor@mail.uft.edu.br>
2024-08-20 16:09:25 -0300

    add endpoints to book model

--- main.py
+++ main.py
@@ -52,3 +52,19 @@ def read_author(author_id: int, db: Session = Depends(get_db)):
 @app.post("/api/authors/", response_model=schemas.Author)
 def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
     return crud.create_author(db=db, author=author)
+
+# books
+@app.get("/api/books/", response_model=list[schemas.Book])
+def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_book(db, skip=skip, limit=limit)
+
+@app.get("/api/books/{book_id}", response_model=schemas.Book)
+def read_book(book_id: int, db: Session = Depends(get_db)):
+    db_book = crud.get_book(db, book_id=book_id)
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return db_book
+
+@app.post("/api/book/", response_model=schemas.Book)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    return crud.create_book(db=db, book=book)
