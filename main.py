2b265fafba042d3f825415653bd6f159b8b6b2ab
Gabriel Barbosa <gabriel.vitor@mail.uft.edu.br>
2024-08-20 16:03:08 -0300

    add endpoint to get an author

--- main.py
+++ main.py
@@ -42,6 +42,13 @@ def create_client(client: schemas.ClientCreate, db: Session = Depends(get_db)):
 def read_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_authors(db, skip=skip, limit=limit)
 
+@app.get("/api/authors/{author_id}", response_model=schemas.Author)
+def read_author(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.get_author(db, author_id=author_id)
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return db_author
+
 @app.post("/api/authors/", response_model=schemas.Author)
 def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
     return crud.create_author(db=db, author=author)
